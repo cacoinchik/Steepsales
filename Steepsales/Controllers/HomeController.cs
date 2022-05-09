ea2dd31e28feb7aeb97bb12055162d4d0dafe73e
cacoinchik <mr.ruslan126@mail.ru>
2022-05-09 12:52:19 +0300

    Оптимизация кода

--- Steepsales/Controllers/HomeController.cs
+++ Steepsales/Controllers/HomeController.cs
@@ -1,17 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Steepsales.Models.EntityFramework;
-using System.IO;
-using System.Linq;
 
 namespace Steepsales.Controllers
 {
     public class HomeController : Controller
     {
-        AppDbContext db;
-        public HomeController(AppDbContext db)
-        {
-            this.db= db;
-        }
         public IActionResult Index()
         {
             return View();
@@ -19,10 +12,6 @@ namespace Steepsales.Controllers
         public IActionResult Faq()
         {
             return View();
-        }
-        public IActionResult Choice()
-        {
-            return View(db.ProductItems.ToList());
         }
     }
 }
