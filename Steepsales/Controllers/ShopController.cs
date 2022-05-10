1804d577a4956a026b13fb9fd14e4e2c55487cd1
cacoinchik <mr.ruslan126@mail.ru>
2022-05-10 12:24:12 +0300

    Добавлена Корзина в меню навигации. Добавлен дизайн для кнопок сортировки товара

--- Steepsales/Controllers/ShopController.cs
+++ Steepsales/Controllers/ShopController.cs
@@ -2,15 +2,18 @@
 using Steepsales.Models.EntityFramework;
 using Steepsales.Models;
 using System;
+using System.Linq;
 
 namespace Steepsales.Controllers
 {
     public class ShopController : Controller 
     {
         private readonly DataManager dataManager;
-        public ShopController(DataManager dataManager)
+        private readonly AppDbContext db;
+        public ShopController(DataManager dataManager,AppDbContext db)
         {
             this.dataManager = dataManager;
+            this.db = db;
         }
         public IActionResult Catalog(Guid id)
         {
@@ -18,7 +21,7 @@ namespace Steepsales.Controllers
             {
                 return View("ShowTovar", dataManager.ProductItem.GetProductItemById(id));
             }
-            return View(dataManager.ProductItem.GetProductItems());
+            return View(db.ProductItems.ToList());
         }
     }
 }
