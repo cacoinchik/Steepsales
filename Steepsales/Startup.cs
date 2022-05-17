35378a9f4e9c96c6ec31e33c27de185e004d72fa
cacoinchik <mr.ruslan126@mail.ru>
2022-05-17 19:40:49 +0300

    Улучшен дизайн панели администратора.

--- Steepsales/Startup.cs
+++ Steepsales/Startup.cs
@@ -16,7 +16,7 @@ namespace Steepsales
     public class Startup
     {
         public IConfiguration Configuration { get; }
-        public Startup(IConfiguration configuration) =>Configuration = configuration;
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
             Configuration.Bind("Project", new Config());
