4444689ef102f818e9e5d1c1384f17668acafe52
cacoinchik <mr.ruslan126@mail.ru>
2022-05-18 17:16:20 +0300

    Добавлена возможность просматривать отправленные заказы.

---
+++ Steepsales/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Steepsales.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Steepsales.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        AppDbContext db;
+        public OrderController(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public class ViewModel
+        {
+            public IEnumerable<Order> Orders { get; set; }
+            public IEnumerable<OrderDetail> Details { get; set; }
+        }
+
+        public IActionResult Show()
+        {
+            var model = new ViewModel()
+            {
+                Orders = db.Orders.ToList(),
+                Details = db.OrderDetails.ToList()
+            };
+            return View(model);
+        }
+    }
+}
